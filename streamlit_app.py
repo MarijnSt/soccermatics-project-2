d7b71c0e72eab9bcd06bb0459fccd1988eed3226
MarijnSt <marijn.stammeleer@gmail.com>
2025-08-31 11:14:57 +0200

    feat: init streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# PAGE CONFIG
+st.set_page_config(
+    page_title="Danger passers",
+    page_icon="⚽️",
+)
+
+st.title('Most Dangerous Passers in the Premier League 2024/2025')
