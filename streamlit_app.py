12191815859fdedf98765c5209aef1d33b802814
MarijnSt <marijn.stammeleer@gmail.com>
2025-09-03 15:00:04 +0200

    feat: add role and minutes filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,28 @@ import numpy as np
 import plotly.graph_objects as go
 from src.styles import style_config
 
+# Functions
+@st.cache_data
+def filter_player_stats(df_player_stats, role_filter, minutes_played_filter):
+    df = df_player_stats.copy()
+
+    # Role filter
+    if role_filter != "All":
+        if role_filter == "Goalkeepers":
+            df = df[df["role"] == "Goalkeeper"]
+        elif role_filter == "Defenders":
+            df = df[df["role"] == "Defender"]
+        elif role_filter == "Midfielders":
+            df = df[df["role"] == "Midfielder"]
+        elif role_filter == "Forwards":
+            df = df[df["role"] == "Forward"]
+
+    # Minutes played filter
+    if minutes_played_filter:
+        df = df[df["minutes_played"] >= minutes_played_filter]
+
+    return df
+
 # PAGE CONFIG
 st.set_page_config(
     page_title="Danger passers",
@@ -15,15 +37,29 @@ st.title('Most dangerous passers in the Premier League 2024/2025')
 # Import data
 df_summary = pd.read_csv('data/summary.csv')
 
+# Sidebar filters
+with st.sidebar:
+    st.subheader("Filter players")
+    role_filter = st.segmented_control(
+        "Role", 
+        ["All", "Goalkeepers", "Defenders", "Midfielders", "Forwards"],
+        default="All"
+    )
+    minutes_played_filter = st.number_input("Minimum minutes played", min_value=400, max_value=10000, value=400, step=1)
+
+# Filter data
+df_filtered = filter_player_stats(df_summary, role_filter, minutes_played_filter)
+
 # Display data
-#st.dataframe(df_summary)
+st.dataframe(df_filtered)
+
 
 # Create the figure
 fig = go.Figure()
 
 # Add traces for each role
-for role in df_summary['role'].unique():
-    role_data = df_summary[df_summary['role'] == role]
+for role in df_filtered['role'].unique():
+    role_data = df_filtered[df_filtered['role'] == role]
     
     fig.add_trace(go.Scatter(
         x=role_data['xD_per_90'],
